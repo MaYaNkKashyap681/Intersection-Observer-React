9ab73a48a44bfbadc825d2b3b8e09b5f4ed312bc
Mayank Wised <mayank@wisedinc.com>
2023-10-25 17:02:15 +0530

    Adding more advanced Pdf View with thumbnail

--- src/pdf/App.jsx
+++ src/pdf/App.jsx
@@ -1,13 +1,8 @@
 import PdfRendered from "./components/PdfRendered"
 const App = () => {
   return (
-    <div className="flex overflow-hidden">
-      <div className="w-[70%]">
-        <PdfRendered />
-      </div>
-      <div className="w-[30%] bg-green-200"> 
-
-      </div>
+    <div className="overflow-hidden">
+      <PdfRendered />
     </div >
   )
 }
--- src/pdf/components/PdfRendered.jsx
+++ src/pdf/components/PdfRendered.jsx
@@ -1,46 +1,51 @@
-import React, { useState } from 'react'
-import { Document, Page, pdfjs } from 'react-pdf'
-import 'react-pdf/dist/Page/AnnotationLayer.css'
-import 'react-pdf/dist/Page/TextLayer.css'
-import pdfFile from '../../assets/ui-color-palette.pdf'
-import { useResizeDetector } from 'react-resize-detector'
-import Loader from './../components/Loader'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+import React, { useState } from 'react';
+import { Document, Page, Thumbnail, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import pdfFile from '../../assets/ui-color-palette.pdf';
+import { useResizeDetector } from 'react-resize-detector';
+import Loader from './../components/Loader';
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfRendered = () => {
+    const [numPages, setNumPages] = useState(0);
+    const [currPage, setCurrPage] = useState(1);
+    const [scale, setScale] = useState(1);
+    const [rotation, setRotation] = useState(0);
+    const [renderedScale, setRenderedScale] = useState(null);
+    const isLoading = renderedScale !== scale;
 
-    const [numPages, setNumPages] = useState(0)
-    const [currPage, setCurrPage] = useState(1)
-    const [scale, setScale] = useState(1)
-    const [rotation, setRotation] = useState(0)
-    const [renderedScale, setRenderedScale] = useState(null)
-    const isLoading = renderedScale !== scale
-
-    const { width, ref } = useResizeDetector()
+    const { width, ref } = useResizeDetector();
 
     return (
-        <div className='flex h-screen'>
-            <div className='w-[30%] h-full bg-yellow-200  overflow-scroll scrollbar-hide'>
+        <div className='flex-1 w-full  mx-auto h-screen '>
 
-            </div>
-            <div className='w-[70%] flex-1 h-full mx-auto  overflow-scroll scrollbar-hide'>
-                <div ref={ref} className='w-full mx-auto '>
-                    <Document
-                        loading={
-                            <div className='h-[5rem] w-full'>
-                                <Loader />
+            <Document
+                loading={
+                    <div className='h-[5rem] w-full'>
+                        <Loader />
+                    </div>
+                }
+                onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+                file={pdfFile}
+                className='max-h-full w-full'
+            >
+                <div className='flex w-full h-full'>
+                    <div className='w-[30%] h-full   max-h-screen overflow-scroll scrollbar-hide'>
+                        {new Array(numPages).fill(0).map((_, index) => (
+                            <div
+                                key={index}
+                                onClick={() => setCurrPage(index + 1)}
+                                className={`w-[50%] bg-white p-1 relative my-4 min-h-[200px] mx-auto overflow-hidden rounded-md border-[3px]  cursor-pointer ${currPage === index + 1 ? 'border-blue-600' : ''
+                                    }`}
+                            >
+                                <div className={`absolute z-[20] bg-blue-600 rounded-br-lg top-0 left-0 p-4 text-white font-bold ${currPage === index + 1 ? '' : 'hidden'}`}>{index + 1}</div>
+                                <Thumbnail pageIndex={index} width={200} height={200} className={`h-[200px] w-[200px]`} />
                             </div>
-                        }
-                        // onLoadError={() => {
-                        //   alert('Error While Loading the PDF')
-                        // }}
-                        onLoadSuccess={({ numPages }) =>
-                            setNumPages(numPages)
-                        }
-                        file={pdfFile}
-                        className='max-h-full w-full'
-                    >
+                        ))}
+                    </div>
+                    <div className='w-[50%] max-h-screen overflow-scroll scrollbar-hide' ref={ref}>
                         {isLoading && renderedScale ? (
                             <Page
                                 width={width ? width : 1}
@@ -51,31 +56,26 @@ const PdfRendered = () => {
                             />
                         ) : null}
 
-                        {
-                            new Array(numPages).fill(0).map((_, index) => (
-                                <Page
-                                    width={width ? width : 1}
-                                    pageNumber={index + 1}
-                                    scale={scale}
-                                    rotate={rotation}
-                                    // key={'@' + scale}
-                                    loading={
-                                        <div className='h-[5rem] w-full'>
-                                            <Loader />
-                                        </div>
-                                    }
-                                    onRenderSuccess={() =>
-                                        setRenderedScale(scale)
-                                    }
-                                />
-                            ))
-                        }
-
-                    </Document>
+                        {new Array(numPages).fill(0).map((_, index) => (
+                            <Page
+                                width={width ? width : 1}
+                                pageNumber={index + 1}
+                                scale={scale}
+                                rotate={rotation}
+                                loading={
+                                    <div className='h-[5rem] w-full'>
+                                        <Loader />
+                                    </div>
+                                }
+                                onRenderSuccess={() => setRenderedScale(scale)}
+                            />
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
-}
+            </Document>
 
-export default PdfRendered
+        </div>
+    );
+};
+
+export default PdfRendered;
